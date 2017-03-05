f232ee48deea5e70ff605a733c5409c40baa3b1a
Holy-Knight <vnycall74@naver.com>
2017-03-05 22:27:13 +0900

    뭔지 모르겠다

--- app/src/main/java/com/haeyum/pcm_citizen/MainActivity.java
+++ app/src/main/java/com/haeyum/pcm_citizen/MainActivity.java
@@ -10,6 +10,7 @@ import android.graphics.Color;
 import android.media.Image;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
